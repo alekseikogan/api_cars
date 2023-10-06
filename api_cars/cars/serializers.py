61bbf21639c48a795304da1d08c7770813691ae0
Алексей Коган (рабочий) <alekseikogan@yandex.ru>
2023-10-06 12:36:09 +0300

    create/update car

---
+++ api_cars/cars/serializers.py
@@ -0,0 +1,38 @@
+from unicodedata import name
+from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+from .models import Car, Mark
+
+
+# class CarSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Car
+#         fields = ('__all__')
+
+class CarSerializer(serializers.Serializer):
+    mark = serializers.CharField(max_length=100)
+    model = serializers.CharField(max_length=100)
+    body = serializers.CharField(max_length=100)
+    description = serializers.CharField()
+    year = serializers.IntegerField()
+    time_create = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        mark = validated_data.get('mark')
+        validated_data['mark'] = Mark.objects.filter(name=mark)[0]
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        mark = validated_data.get('mark', instance.mark)
+        instance.mark = Mark.objects.filter(name=mark)[0]
+        instance.model = validated_data.get('model', instance.model)
+        instance.body = validated_data.get('body', instance.body)
+        instance.description = validated_data.get(
+            'description',
+            instance.description)
+        instance.year = validated_data.get('year', instance.year)
+        instance.time_create = validated_data.get(
+            'time_create',
+            instance.time_create)
+        instance.save()
+        return instance
--- api_cars/cars/views.py
+++ api_cars/cars/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.forms import model_to_dict
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Car, Mark
+from .serializers import CarSerializer
+
+# class CarAPIView(generics.ListAPIView):
+#     queryset = Car.objects.all()
+#     serializer_class = CarSerializer
+
+
+class CarAPIView(APIView):
+    def get(self, request):
+        cars = Car.objects.all()
+        return Response({'cars': CarSerializer(cars, many=True).data})
+
+    def post(self, request):
+        serializer = CarSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'car': serializer.data})
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        print(f'pk={pk}')
+        if not pk:
+            return Response({'error': 'Метод PUT не определен!!!'})
+        try:
+            instance = Car.objects.get(pk=pk)
+            print(f'instance={instance}')
+        except:
+            return Response({'error': 'Запсиь не найдена'})
+
+        serializer = CarSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'car': serializer.data})

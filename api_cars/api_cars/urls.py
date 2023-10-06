a436d25a38297b31cac1d4e7587b61be951062bc
Алексей Коган (рабочий) <alekseikogan@yandex.ru>
2023-10-06 16:00:31 +0300

    actions lexus

--- api_cars/api_cars/urls.py
+++ api_cars/api_cars/urls.py
@@ -1,9 +1,14 @@
-from cars.views import CarAPIView
+from cars.views import CarViewSet
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
+router.register(r'cars', CarViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/cars/', CarAPIView.as_view()),
-    path('api/v1/cars/<int:pk>/', CarAPIView.as_view())
+    path('api/v1/', include(router.urls)),
 ]
+
+print(f'Все урлы = {router.urls}')
--- api_cars/cars/views.py
+++ api_cars/cars/views.py
@@ -1,38 +1,55 @@
-from django.forms import model_to_dict
+from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .models import Car, Mark
 from .serializers import CarSerializer
 
-# class CarAPIView(generics.ListAPIView):
+
+class CarViewSet(viewsets.ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+
+    @action(methods=['get'], detail=False)
+    def lexus(self, request):
+        querry = Car.objects.filter(mark__name='Lexus')
+        print(f'querry = {querry}')
+        serializer = self.get_serializer(querry, many=True)
+        return Response(serializer.data)
+
+
+# class CarAPIList(generics.ListCreateAPIView):
 #     queryset = Car.objects.all()
 #     serializer_class = CarSerializer
 
 
-class CarAPIView(APIView):
-    def get(self, request):
-        cars = Car.objects.all()
-        return Response({'cars': CarSerializer(cars, many=True).data})
+# class CarAPIUpdate(generics.UpdateAPIView):
+#     queryset = Car.objects.all()
+#     serializer_class = CarSerializer
 
-    def post(self, request):
-        serializer = CarSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'car': serializer.data})
+# class CarAPIView(APIView):
+#     def get(self, request):
+#         cars = Car.objects.all()
+#         return Response({'cars': CarSerializer(cars, many=True).data})
 
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get('pk', None)
-        print(f'pk={pk}')
-        if not pk:
-            return Response({'error': 'Метод PUT не определен!!!'})
-        try:
-            instance = Car.objects.get(pk=pk)
-            print(f'instance={instance}')
-        except:
-            return Response({'error': 'Запсиь не найдена'})
+#     def post(self, request):
+#         serializer = CarSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({'car': serializer.data})
 
-        serializer = CarSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'car': serializer.data})
+#     def put(self, request, *args, **kwargs):
+#         pk = kwargs.get('pk', None)
+#         print(f'pk={pk}')
+#         if not pk:
+#             return Response({'error': 'Метод PUT не определен!!!'})
+#         try:
+#             instance = Car.objects.get(pk=pk)
+#             print(f'instance={instance}')
+#         except:
+#             return Response({'error': 'Запсиь не найдена'})
+
+#         serializer = CarSerializer(data=request.data, instance=instance)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({'car': serializer.data})
